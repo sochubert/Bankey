523f6d045008d35aa1836bae1ec1fe86a19b9c95
sochubert <sochubert@gmail.com>
2022-07-09 21:39:27 +0900

    Creating the LoginView [programmatically]

---
+++ Bankey/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Minjae Lee on 2022/07/09.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            // 1x = 8pts
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
