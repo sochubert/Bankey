5b7cce56e2be10196755ce6425503f6dcbe0819b
sochubert <sochubert@gmail.com>
2022-07-09 21:16:37 +0900

    Init

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Minjae Lee on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
