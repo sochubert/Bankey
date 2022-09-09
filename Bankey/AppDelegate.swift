be3ab5751c61002dfffe28bdb62b3811657a5f09
sochubert <sochubert@gmail.com>
2022-09-09 16:46:13 +0900

    Show onboarding only once

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -49,12 +49,17 @@ extension AppDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        setRootViewController(onboardingContainerViewController)
+        if LocalState.hasOnboarded {
+            setRootViewController(dummyViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
     }
 }
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
+        LocalState.hasOnboarded = true
         setRootViewController(dummyViewController)
     }
 }
